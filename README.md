8f8fd0efb16a4680d19516aa3778f239720fd01c
jiayankai <jiayankai@gridsum.com>
2018-01-10 11:37:15 +0800

    npm run init command
    add command : npm run init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gs-vue-cli
+
+### version
+1.0.0
+
+### init
+`npm run init`
--- command/init.js
+++ command/init.js
@@ -6,34 +6,34 @@ const config = require('../templates')
 const chalk = require('chalk')
 
 module.exports = () => {
- co(function *() {
-    // 处理用户输入
-      // let tplName = yield prompt('Template name: ')
-      let projectName = yield prompt('Project name: ')
-      // let projectName = 'frontend-template';
-      let gitUrl = 'https://github.com/jiantao2017/gs-vue-template.git';
-      let branch = 'master';
+    co(function *() {
+        // 处理用户输入
+        // let tplName = yield prompt('Template name: ')
+        let projectName = yield prompt('Project name: ')
+        // let projectName = 'frontend-template';
+        let gitUrl = 'https://github.com/jiantao2017/gs-vue-template.git';
+        let branch = 'master';
 
-    // if (!config.tpl[tplName]) {
-    //     console.log(chalk.red('\n × Template does not exit!'))
-    //     process.exit()
-    // }
-    // gitUrl = config.tpl[tplName].url
-    // branch = config.tpl[tplName].branch
+        // if (!config.tpl[tplName]) {
+        //     console.log(chalk.red('\n × Template does not exit!'))
+        //     process.exit()
+        // }
+        // gitUrl = config.tpl[tplName].url
+        // branch = config.tpl[tplName].branch
 
-    // git命令，远程拉取项目并自定义项目名
-    let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
+        // git命令，远程拉取项目并自定义项目名
+        let cmdStr = `git clone ${gitUrl} ${projectName} && cd ${projectName} && git checkout ${branch}`
 
-    console.log(chalk.white('\n Start generating...'))
+        console.log(chalk.white('\n Init project template...'))
 
-    exec(cmdStr, (error, stdout, stderr) => {
-      if (error) {
-        console.log(error)
-        process.exit()
-      }
-      console.log(chalk.green('\n √ Generation completed!'))
-      console.log(`\n cd ${projectName} && npm install \n`)
-      process.exit()
+        exec(cmdStr, (error, stdout, stderr) => {
+            if (error) {
+                console.log(error)
+                process.exit()
+            }
+            console.log(chalk.green('\n √ Init completed!'))
+            console.log(`\n cd ${projectName} && npm install \n`)
+            process.exit()
+        })
     })
-  })
 }
